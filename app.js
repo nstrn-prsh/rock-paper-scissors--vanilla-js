6861bf91f2e2e38f9cf7c4ce628ed3263c362d43
nastaran poursharif <nastaran.p.sh.77@gmail.com>
2021-08-20 21:46:14 +0430

    win lose tie

--- app.js
+++ app.js
@@ -1,39 +1,60 @@
 // variables
-let userScore = 0
-let pcScore = 0
+let userScore = 0;
+let pcScore = 0;
 
-const rock_div = document.querySelector('#rock')
-const paper_div = document.querySelector('#paper')
-const scissors_div = document.querySelector('#scissors')
+const rock_div = document.querySelector("#rock");
+const paper_div = document.querySelector("#paper");
+const scissors_div = document.querySelector("#scissors");
 
 // eventListeners
 //  new: eventListeners toye function tarif mikonim ke motmaen shim kar mikone
-eventListeners()
-function eventListeners(){
-    rock_div.addEventListener('click',()=>{
-        // console.log('rock');
-        game('r')
-    })
-    paper_div.addEventListener('click',()=>{
-        // console.log('paper');
-        game('p')
-    })
-    scissors_div.addEventListener('click',()=>{
-        // console.log('scissors');
-        game('s')
-    })
+eventListeners();
+function eventListeners() {
+     rock_div.addEventListener("click", () => {
+          // console.log('rock');
+          game("r");
+     });
+     paper_div.addEventListener("click", () => {
+          // console.log('paper');
+          game("p");
+     });
+     scissors_div.addEventListener("click", () => {
+          // console.log('scissors');
+          game("s");
+     });
 }
 
 // functions
 // get pc choice
-function getPcChoice(){
-    const choices = ['r','p','s']
-    const randomNum = Math.floor(Math.random()*3)
-    return choices[randomNum]
+function getPcChoice() {
+     const choices = ["r", "p", "s"];
+     const randomNum = Math.floor(Math.random() * 3);
+     return choices[randomNum];
 }
 
 // compare user and ps choice
-function game(userChoice){
-    console.log(`user : ${userChoice}`);
-    console.log(`computer : ${getPcChoice()}`);
+function game(userChoice) {
+     // console.log(`user : ${userChoice}`);
+     // console.log(`computer : ${getPcChoice()}`);
+     const pcChoice = getPcChoice();
+
+     switch (userChoice + pcChoice) {
+          case "rs":
+          case "pr":
+          case "sp":
+               console.log("You Won!");
+               break;
+          case "rp":
+          case "sr":
+          case "ps":
+               console.log("You Lost!");
+               break;
+          case "rr":
+          case "pp":
+          case "ss":
+               console.log("You Tied!");
+               break;
+          default:
+               break;
+     }
 }
