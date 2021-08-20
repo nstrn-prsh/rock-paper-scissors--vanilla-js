7b475969d7e2ecf5f927d4d896c7763191e1dfb5
nastaran poursharif <nastaran.p.sh.77@gmail.com>
2021-08-21 00:58:53 +0430

    ul changes based on result

--- app.js
+++ app.js
@@ -6,13 +6,16 @@ const rock_div = document.querySelector("#rock");
 const paper_div = document.querySelector("#paper");
 const scissors_div = document.querySelector("#scissors");
 
-const userScore_span = document.getElementById('user-score')
-const pcScore_span = document.getElementById('comp-score')
+const userScore_span = document.getElementById("user-score");
+const pcScore_span = document.getElementById("comp-score");
+
+const result_div = document.querySelector(".result p");
 
 // eventListeners
 //  new: eventListeners toye function tarif mikonim ke motmaen shim kar mikone
 eventListeners();
 function eventListeners() {
+    // userChoice is here
      rock_div.addEventListener("click", () => {
           // console.log('rock');
           game("r");
@@ -45,38 +48,60 @@ function game(userChoice) {
           case "rs":
           case "pr":
           case "sp":
-               win();
+               win(userChoice, pcChoice);
                break;
           case "rp":
           case "sr":
           case "ps":
-               lose();
+               lose(userChoice, pcChoice);
                break;
           case "rr":
           case "pp":
           case "ss":
-               tie();
+               tie(userChoice, pcChoice);
                break;
           default:
                break;
      }
 }
 
-function win() {
-    //  console.log("You Won!");
-    userScore ++
-    userScore_span.innerHTML = userScore
-    pcScore_span.innerHTML = pcScore
+// baraye inke be sorate kalame neshon bedim ki chio entekhab kard
+function convertToWord(letter) {
+     if (letter == "r") return "rock";
+     else if (letter == "p") return "paper";
+     else return "scissors";
 }
 
-function lose() {
-    //  console.log("You Lost!");
-    pcScore ++
-    userScore_span.innerHTML = userScore
-    pcScore_span.innerHTML = pcScore
+function classStatus(userChoice, color){
+   //  inja az convertToWord estefade mikonim chon id ha be sorat kalame kamel neveshte shodan 
+    // vali ma baraye rahati faghat az harfe avaleshon estefade kardim
+    document.getElementById(convertToWord(userChoice)).classList.add(color)
+    //remove after 1000 ms
+    setTimeout(() => {
+        document.getElementById(convertToWord(userChoice)).classList.remove(color)
+    }, 1000);
 }
 
-function tie() {
-    //  console.log("You Tied!");
+function win(userChoice, pcChoice) {
+     //  console.log("You Won!");
+     userScore++;
+     userScore_span.innerHTML = userScore;
+     pcScore_span.innerHTML = pcScore;
+     result_div.innerHTML = `you won! you chose ${convertToWord(userChoice)} and robot chose ${convertToWord(pcChoice)}.`
+    classStatus(userChoice, 'green')
+}
 
+function lose(userChoice, pcChoice) {
+     //  console.log("You Lost!");
+     pcScore++;
+     userScore_span.innerHTML = userScore;
+     pcScore_span.innerHTML = pcScore;
+     result_div.innerHTML = `you lost! you chose ${convertToWord(userChoice)} and robot chose ${convertToWord(pcChoice)}.`
+     classStatus(userChoice, 'red')
+    }
+
+function tie(userChoice, pcChoice) {
+     //  console.log("You Tied!");
+     result_div.innerHTML = `you tied! you chose ${convertToWord(userChoice)} and robot chose ${convertToWord(pcChoice)}.`
+     classStatus(userChoice, 'gray')
 }
